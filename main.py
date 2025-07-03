5d2c851b73d2ae08c3c71a890a3a3853b30f4009
ZHSC17 <32410651+ZHSC17@users.noreply.github.com>
2025-07-03 22:37:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ def plot_multiple_y_axes(data, x_column, y_columns, chart_type):
     color_cycle = cycle(colors)
 
     axes = [host]
-    host.set_xlabel(x_column)
-    host.set_ylabel(y_columns[0], color=colors[0])
+    host.set_xlabel(x_column, fontproperties=my_font)
+    host.set_ylabel(y_columns[0], color=colors[0], fontproperties=my_font)
     host.tick_params(axis='y', labelcolor=colors[0])
 
     # 画第一个Y轴
@@ -56,12 +56,12 @@ def plot_multiple_y_axes(data, x_column, y_columns, chart_type):
         elif chart_type == "散点图":
             ax_new.scatter(data[x_column], data[y_columns[i]], color=color, label=y_columns[i])
 
-        ax_new.set_ylabel(y_columns[i], color=color)
+        ax_new.set_ylabel(y_columns[i], color=color, fontproperties=my_font)
         ax_new.tick_params(axis='y', labelcolor=color)
 
     # 设置图表标题和布局
-    plt.title(f'多Y轴图表: {x_column} vs ' + ' & '.join(y_columns) , fontproperties=my_font)
-    plt.xticks(rotation=90, ha='right')
+    plt.title(f'多Y轴图表: {x_column} vs ' + ' & '.join(y_columns) )
+    plt.xticks(rotation=90, ha='right', fontproperties=my_font)
     fig.autofmt_xdate(rotation=90)
     plt.subplots_adjust(right=0.2 + 0.05 * (len(y_columns)-2))  # 自动调宽图表，避免轴标签被遮挡
     fig.tight_layout()
